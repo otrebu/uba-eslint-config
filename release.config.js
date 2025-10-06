44ba1b4dd9cec9666028082d192b7f9d623aee85
Uberto Rapizzi <Uberto.Rapizzi@jtglobal.com>
2025-10-06 14:41:48 +0100

    chore: update pnpm-lock.yaml

--- release.config.js
+++ release.config.js
@@ -25,6 +25,7 @@ export default {
       {
         assets: ["CHANGELOG.md", "package.json"],
         message:
+          // eslint-disable-next-line no-template-curly-in-string
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
     ],
